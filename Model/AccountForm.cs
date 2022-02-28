1fda76f6973b026a9fc45c59e4d5582d5a309f71
Dustu <35203971+qhl088@users.noreply.github.com>
2022-02-28 17:02:02 +1300

    Create AccountForm.cs

---
+++ Model/AccountForm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAuto1
+{
+    public class AccountForm
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int number { get; set; }
+        public string info { get; set; }
+        public string error { get; set; }
+    }
+}
