db57bf0191c330d2848288493d33c8448da8532d
Dustu <35203971+qhl088@users.noreply.github.com>
2022-02-28 17:04:26 +1300

    Add files via upload

---
+++ Password/registerinfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAuto1.Password
+{
+    public class registerinfo
+    {
+        public string Email;
+        public string Name;
+        public string Password;
+        public string Code;
+        
+        //public string Error
+        //{
+        //    get;
+        //}
+    }
+}
