c5c6de8c3247c3b2e419f5eed9bb5e28e50522ea
Dustu <35203971+qhl088@users.noreply.github.com>
2022-02-28 17:04:14 +1300

    Create invalidpassword.cs

---
+++ Password/invalidpassword.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAuto1.Password
+{
+    public class invalidpassword
+    {
+        public string Name;
+        public string Password;
+    }
+}
